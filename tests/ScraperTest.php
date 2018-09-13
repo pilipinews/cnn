8bb9695ef4c00fb5d505b24f299a917a4fc9dd99
Rougin Gutib <rougingutib@gmail.com>
2018-09-14 02:27:17 +0800

    Initial commit

---
+++ tests/ScraperTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pilipinews\Website\Cnn;
+
+/**
+ * Scraper Test
+ *
+ * @package Pilipinews
+ * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ */
+class ScraperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests ScraperInterface::scrape.
+     *
+     * @return void
+     */
+    public function testScrapeMethod()
+    {
+    }
+}
